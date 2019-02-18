430f035f618a01ce2f1857f072e16f4e9a5c91a6
Patrice Gedeon <pgedeon84@gmail.com>
2019-02-18 17:42:25 -0500

    completed coding for ATV controller and views

--- app/controllers/atvs_controller.rb
+++ app/controllers/atvs_controller.rb
@@ -1,2 +1,29 @@
 class AtvsController < ApplicationController
+
+  def index
+    @atvs = Atv.all
+  end
+
+  def show
+    @atv = Atv.find(params[:id])
+  end
+
+  def new
+    @atv = Atv.new
+  end
+
+  def create
+    @atv = Atv.new(atv_params)
+    if @atv.save
+      redirect_to atv_path(@atv)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def atv_params
+    params.require(:atvs).permit(:make, :model, :year, :mileage, :price)
+  end
 end
