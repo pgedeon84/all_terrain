e15380d4ab8b4a5e8437edfb2cd992aa929c6f1a
Patrice Gedeon <pgedeon84@gmail.com>
2019-02-19 17:41:29 -0500

    coded show page

--- app/models/atv.rb
+++ app/models/atv.rb
@@ -1,4 +1,9 @@
 class Atv < ApplicationRecord
   belongs_to :user
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :city, presence: true
+  validates :description, presence: true
   mount_uploader :photo, PhotoUploader
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :atv
+  validates :from, presence: true
+  validates :to, presence: true
 end
