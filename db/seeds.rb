8406855540b489cbbefca090fa112eae3c7f3a3c
Patrice Gedeon <pgedeon84@gmail.com>
2019-02-21 10:03:08 -0500

    background img

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,65 +9,9 @@
 Atv.destroy_all
 User.destroy_all
 
-
 url = "http://media.nj.com/essex_impact/photo/2015/05/29/ga1025atv-6-kurdzuk-9ccf6173101256d7.jpg"
 puts "creating users"
-1.times do
-  number = rand(40...80)
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
-  )
-end
-
-url = "http://atvillustrated.com/files/2015.drr_.drx90.black_.front-right.riding.on-track.jpg"
-puts "creating users"
-1.times do
-  number = rand(40...80)
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
-  )
-end
-
-url = "http://atvillustrated.com/files/2014.polaris.outlaw90.front-right.blue_.riding.on-dirt.jpg"
-puts "creating users"
-1.times do
-  number = rand(40...80)
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
-  )
-end
-
-url = "http://atvillustrated.com/files/2014.polaris.outlaw90.front-right.blue_.riding.on-dirt.jpg"
-puts "creating users"
-1.times do
-  number = rand(40...80)
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
-  )
-end
-
-url = "https://www.whatsup-chiangmai.com/wp-content/uploads/2017/04/atv-rides.jpg"
-puts "creating users"
-1.times do
-  number = rand(40...80)
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
-  )
-end
-
-url = "https://f-static.motosport.com/motoblog/2014/ATV-Tips-gear.jpg"
-puts "creating users"
-1.times do
+10.times do
   number = rand(40...80)
   User.create!(
     email: Faker::Internet.email,
