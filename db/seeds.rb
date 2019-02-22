794c78f23ba5fb50672d6b9c3b1a9b035c9e5cea
nohat yacinthe <nohatyacinthe1@hotmail.fr>
2019-02-22 13:05:04 -0500

    wtf

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Atv.destroy_all
-User.destroy_all
+# Atv.destroy_all
+# User.destroy_all
 
-url = "http://media.nj.com/essex_impact/photo/2015/05/29/ga1025atv-6-kurdzuk-9ccf6173101256d7.jpg"
-puts "creating users"
-10.times do
-  number = rand(40...80)
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
-  )
-end
-puts "creating atvs"
+# url = "http://media.nj.com/essex_impact/photo/2015/05/29/ga1025atv-6-kurdzuk-9ccf6173101256d7.jpg"
+# puts "creating users"
+# 10.times do
+#   number = rand(40...80)
+#   User.create!(
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password,
+#     remote_photo_url: "https://randomuser.me/api/portraits/men/#{number}.jpg"
+#   )
+# end
+# puts "creating atvs"
 
 
-User.all.each do |user|
-  Atv.create!(
-      make: Faker::Vehicle.make,
-      model:Faker::Vehicle.model,
-      year: rand(1995..2019),
-      price: rand(50..200),
-      user: user,
-      city: Faker::Nation.capital_city,
-      remote_photo_url: url,
-      description: "Dolore deserunt consequat quis nulla aute duis ex esse dolore nisi exercitation exercitation mollit anim velit consequat ut eu."
-    )
-  end
+# User.all.each do |user|
+#   Atv.create!(
+#       make: Faker::Vehicle.make,
+#       model:Faker::Vehicle.model,
+#       year: rand(1995..2019),
+#       price: rand(50..200),
+#       user: user,
+#       city: Faker::Nation.capital_city,
+#       remote_photo_url: url,
+#       description: "Dolore deserunt consequat quis nulla aute duis ex esse dolore nisi exercitation exercitation mollit anim velit consequat ut eu."
+#     )
+#   end
 
 
 
